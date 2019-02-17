40ce36d993e234c5f53c659fad5c28898ff90d88
aschaaf <schaaf1091@gmail.com>
2019-02-17 13:34:45 -0700

    incorrect guesses log correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // array of random words
-var words = ["word1", "word12", "word123", "word1234", "word12345"];
+var words = ["WORD1", "WORD2", "WORD3", "WORD4", "WORD5"];
 
 // choose a random word
 var word = words[Math.floor(Math.random() * words.length)];
@@ -7,46 +7,54 @@ var word = words[Math.floor(Math.random() * words.length)];
 // empty array to store answer
 var answer = [];
 for (var i = 0; i < word.length; i++) {
-    answer[i] = "_";
+  answer[i] = "_";
 }
 
 // empty array to store letter guesses
 var incorrectGuesses = [];
-
 
 var remainingLetters = word.length;
 
 // start game
 // get the player's guess
 
-document.onkeyup = function (event) {
-    var guess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(guess);
-    console.log(incorrectGuesses);
-    console.log(word);
-    console.log(answer);
+document.onkeyup = function(event) {
+  var guess = String.fromCharCode(event.keyCode).toUpperCase();
+  console.log(guess);
+  console.log(incorrectGuesses);
+  console.log(word);
+  console.log(answer);
 
-    if (word.indexOf(guess) === -1) {
-        incorrectGuesses.push(" " + guess);
-    } else {
-        // update the game stats
+  if (incorrectGuesses.indexOf(guess) === -1 && word.indexOf(guess) === -1) {
+    incorrectGuesses.push(guess);
+  } else {
+    // update the game stats
+    for (var k = 0; k < word.length; k++) {
+      if (word[k] === guess) {
         for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answer[j] = guess;
-                remainingLetters--;
-            }
+          if (word[j] === guess) {
+            answer[j] = guess;
+            remainingLetters--;
+          }
         }
+        answer[k] = guess;
+        remainingLetters--;
+      }
     }
-    // display word in HTML
+  }
+  // display word in HTML
 
-    var displayWord =
-        "<p>" + answer.join(" ") + "</p>" +
-        "<p>Incorrect Guesses: " + incorrectGuesses + "</p>";
+  var displayWord =
+    "<p>" +
+    answer.join(" ") +
+    "</p>" +
+    "<p>Incorrect Guesses: " +
+    incorrectGuesses.join(", ") +
+    "</p>";
 
-    document.querySelector("#display-word").innerHTML = displayWord;
-}
+  document.querySelector("#display-word").innerHTML = displayWord;
+};
 // end of game
-
 
 /* Display the following on the page: */
 
