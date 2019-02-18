cb2407f6866c1cbba1bbc4f46ec7f8ff05d4c876
aschaaf <schaaf1091@gmail.com>
2019-02-17 21:46:56 -0700

    added wins calculator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,13 @@
+// document.querySelector("#answer").innerHTML = answer.join(" ");
+// document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+
+// global variables
+
+var guessesLeft = 15;
+var guesses = [];
+var wins = 0;
+var losses = 0;
+
 // array of random words
 var words = ["WORD1", "WORD2", "WORD3", "WORD4", "WORD5"];
 
@@ -10,66 +20,45 @@ for (var i = 0; i < word.length; i++) {
   answer[i] = "_";
 }
 
-// empty array to store letter guesses
-var incorrectGuesses = [];
+// // start game
 
-var remainingLetters = word.length;
-
-// start game
-// get the player's guess
-
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   var guess = String.fromCharCode(event.keyCode).toUpperCase();
-  console.log(guess);
-  console.log(incorrectGuesses);
-  console.log(word);
-  console.log(answer);
 
-  if (incorrectGuesses.indexOf(guess) === -1 && word.indexOf(guess) === -1) {
-    incorrectGuesses.push(guess);
-  } else {
-    // update the game stats
-    for (var k = 0; k < word.length; k++) {
-      if (word[k] === guess) {
-        for (var j = 0; j < word.length; j++) {
-          if (word[j] === guess) {
-            answer[j] = guess;
-            remainingLetters--;
-          }
+  if (guesses.indexOf(guess) === -1 && word.indexOf(guess) === -1) {
+    guesses.push(guess);
+    guessesLeft--;
+  }
+  // update the game stats
+  for (var k = 0; k < word.length; k++) {
+    if (word[k] === guess) {
+      for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+          answer[j] = guess;
         }
-        answer[k] = guess;
-        remainingLetters--;
       }
     }
   }
+  if (answer.indexOf("_") === -1) {
+    wins++;
+    alert("You win!");
+    confirm("Play again?");
+  }
+  if (guesses === 0) {
+    alert("You lose!");
+    confirm("Play again?");
+  }
+
   // display word in HTML
+  var html =
+    "<p>Wins</p>" +
+    "<p>" + wins + "</p>" +
+    "<p>Current Word</p>" +
+    "<p>" + answer.join(" ") + "</p>" +
+    "<p>Letters Already Guessed: " + guesses.join(" ") + "</p>" +
+    "<p>Number of Guesses Remaining: " + guessesLeft + "</p>";
 
-  var displayWord =
-    "<p>" +
-    answer.join(" ") +
-    "</p>" +
-    "<p>Incorrect Guesses: " +
-    incorrectGuesses.join(", ") +
-    "</p>";
-
-  document.querySelector("#display-word").innerHTML = displayWord;
-};
+  document.querySelector("#html").innerHTML = html;
+  console.log(word.length);
+}
 // end of game
-
-/* Display the following on the page: */
-
-/* Press any key to get started! */
-// Write in HTML file
-
-/* Wins: (# of times user guessed the word correctly). */
-// place inside p tag and assign an id
-
-/* If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`. */
-
-/* As the user guesses the correct letters, reveal them: `m a d o _ _ a`. */
-
-/* Number of Guesses Remaining: (# of guesses remaining for the user). */
-
-/* Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`). */
-
-/* After the user wins/loses the game should automatically choose another word and make the user play it. */
